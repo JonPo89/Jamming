773ce69075b0ca121b73e04e0ef985a3cf46c543
JonPo89 <162661391+JonPo89@users.noreply.github.com>
2024-07-08 14:08:31 +1000

    Update authorisationSlice.js
    Update the redirectUri

--- src/features/authorisation/authorisationSlice.js
+++ src/features/authorisation/authorisationSlice.js
@@ -3,7 +3,7 @@ import {createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
 const clientId = process.env.REACT_APP_CLIENT_ID;
 
-const redirectUri = "https://jammming-porter.netlify.app/";
+const redirectUri = "https://main--jammming-porter.netlify.app/";
 const code = new URLSearchParams(window.location.search).get('code');
 const scope = [
     "user-read-email",
